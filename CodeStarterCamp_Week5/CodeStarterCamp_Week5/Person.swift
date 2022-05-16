52327c9bf9407295f4240298fa6015c43fa9f4d8
JAY-Winter <rubat0@naver.com>
2022-05-16 15:46:46 +0900

    feat: Person type 생성

---
+++ CodeStarterCamp_Week5/CodeStarterCamp_Week5/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  CodeStarterCamp_Week5
+//
+//  Created by JAEHYEON on 2022/05/16.
+//
+
+import Foundation
+
+class Person {
+    var name: String
+    var height: Int
+    
+    init(name: String, height: Int) {
+        self.name = name
+        self.height = height
+    }
+}
