ab388e5fd109a6b4708cb5ca421067f19fbcb5fb
JAY-Winter <rubat0@naver.com>
2022-05-16 11:00:18 +0900

    first commit

---
+++ CodeStarterCamp_Week5/CodeStarterCamp_Week5/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  CodeStarterCamp_Week5
+//
+//  Created by JAEHYEON on 2022/05/16.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# Week5
+
+## 개인 미션 수행 Repository
