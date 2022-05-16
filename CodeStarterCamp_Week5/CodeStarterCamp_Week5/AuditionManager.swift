9e7227bbca96e3bb7e834e2a662d79084262933c
JAY-Winter <rubat0@naver.com>
2022-05-16 18:38:17 +0900

    feat: AudtionManager 구조체 생성

---
+++ CodeStarterCamp_Week5/CodeStarterCamp_Week5/AuditionManager.swift
@@ -0,0 +1,16 @@
+//
+//  AuditionManager.swift
+//  CodeStarterCamp_Week5
+//
+//  Created by JAEHYEON on 2022/05/16.
+//
+
+import Foundation
+
+struct AuditionManager {
+    var totalApplicantsList: [Person]
+    
+    var passedApplicantsList: [Person] = []
+    
+    func cast() { }
+}
